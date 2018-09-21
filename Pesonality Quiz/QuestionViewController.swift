97a8a0f2cde8af8ae5c1ce352b2cf1e4fcee3ffa
zis <desert73eagle@gmail.com>
2018-09-21 14:23:55 +0300

    Added label's outlets

--- Pesonality Quiz/QuestionViewController.swift
+++ Pesonality Quiz/QuestionViewController.swift
@@ -11,11 +11,25 @@ import UIKit
 class QuestionViewController: UIViewController {
     var questionIndex = 0
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
     @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet weak var singleButton1: UIButton!
+    @IBOutlet weak var singleButton2: UIButton!
+    @IBOutlet weak var singleButton3: UIButton!
+    @IBOutlet weak var singleButton4: UIButton!
     
     @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var multiLabel1: UILabel!
+    @IBOutlet weak var multiLabel2: UILabel!
+    @IBOutlet weak var multiLabel3: UILabel!
+    @IBOutlet weak var multiLabel4: UILabel!
     
     @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var rangedLabel1: UILabel!
+    @IBOutlet weak var rangedLabel2: UILabel!
+    
+    @IBOutlet weak var questionProgressView: UIProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
